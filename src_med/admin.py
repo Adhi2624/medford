8fe0b9964c005a3194422fc0d8e6cb31c8f8a6b8
adhi2624 <adhithiyan.2624@sonatech.ac.in>
2023-01-25 22:35:14 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src_med/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import contact,carrer
+
+# Register your models here.
+admin.site.register(contact)
+admin.site.register(carrer)
+
+
+admin.site.site_header="MEDFORD TECHNOLOGIES"
+admin.site.site_title="MEDFORD"
---
+++ src_med/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('home',views.home,name='home'),
+    path('products',views.product,name='products'),
+    path('carrer',views.carrer,name='carrer'),
+    path('aboutus',views.about_us,name='aboutus'),
+    path('contact',views.contact,name='contact'),
+]
